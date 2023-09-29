87f4de6913f599f5e626c5a75873f9581c44b0bb
dearian <dearian2706@gmail.com>
2023-09-29 11:09:03 +0300

    add font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-
-import './App.css'
 import Header from './component/Header';
 import AccountContainer from './component/AccountContainer';
+import './App.css'
 
 function App() {
  
--- src/component/Account/index.tsx
+++ src/component/Account/index.tsx
@@ -5,70 +5,68 @@ function Account() {
     const [expand, setExpand] = useState(false);
     return (
         <div className="account">
-            <div style={{display:'flex'}}>
-                <div className='picture'>Картинка</div>
-                <div className='computer_name'>Computer</div>
-                <button onClick={() => setExpand(!expand)}>Button</button>
-            </div>
-            {expand &&
-            
-      <div className='info'>
-        <ul className='info_list'>
-        <li>
-                <h3>Корпус</h3>
-                <p>Крутий</p>
-            </li>
-            <li>
-                <h3>Корпус</h3>
-                <p>Крутий</p>
-            </li>
-            <li>
-                <h3>Корпус</h3>
-                <p>Крутий</p>
-            </li>
-            <li>
-                <h3>Корпус</h3>
-                <p>Крутий</p>
-            </li>
-            <li>
-                <h3>Проц</h3>
-                <p>Також крутий</p>
-            </li>
-            <li>
-                <h3>Монітор</h3>
-                <p>Також крутий</p>
-            </li>
-        </ul>
-        <ul className='info_list'>
-        <li>
-                <h3>Корпус</h3>
-                <p>Крутий</p>
-            </li>
-            <li>
-                <h3>Корпус</h3>
-                <p>Крутий</p>
-            </li>
-            <li>
-                <h3>Корпус</h3>
-                <p>Крутий</p>
-            </li>
-            <li>
-                <h3>Корпус</h3>
-                <p>Крутий</p>
-            </li>
-            <li>
-                <h3>Проц</h3>
-                <p>Також крутий</p>
-            </li>
-            <li>
-                <h3>Монітор</h3>
-                <p>Також крутий</p>
-            </li>
-        </ul>
+					<div style={{display:'flex'}}>
+						<div className='picture'>Картинка</div>
+						<div className='computer_name'>Computer</div>
+						<button onClick={() => setExpand(!expand)}>Button</button>
+					</div>
+        {expand &&    
+					<div className='info'>
+						<ul className='info_list'>
+							<li>
+									<h3>Корпус</h3>
+									<p>Крутий</p>
+							</li>
+							<li>
+									<h3>Корпус</h3>
+									<p>Крутий</p>
+							</li>
+							<li>
+									<h3>Корпус</h3>
+									<p>Крутий</p>
+							</li>
+							<li>
+									<h3>Корпус</h3>
+									<p>Крутий</p>
+							</li>
+							<li>
+									<h3>Проц</h3>
+									<p>Також крутий</p>
+							</li>
+							<li>
+									<h3>Монітор</h3>
+									<p>Також крутий</p>
+							</li>
+					</ul>
+					<ul className='info_list'>
+					<li>
+									<h3>Корпус</h3>
+									<p>Крутий</p>
+							</li>
+							<li>
+									<h3>Корпус</h3>
+									<p>Крутий</p>
+							</li>
+							<li>
+									<h3>Корпус</h3>
+									<p>Крутий</p>
+							</li>
+							<li>
+									<h3>Корпус</h3>
+									<p>Крутий</p>
+							</li>
+							<li>
+									<h3>Проц</h3>
+									<p>Також крутий</p>
+							</li>
+							<li>
+									<h3>Монітор</h3>
+									<p>Також крутий</p>
+							</li>
+						</ul>
+					</div>
+				}
       </div>
-      }
-    
-        </div>
         
     )
 }
