d67dcb2fb13ae071160f9204e4197f3dd541335f
dearian <dearian2706@gmail.com>
2023-10-02 14:19:55 +0300

    computer params

--- src/component/Account/index.tsx
+++ src/component/Account/index.tsx
@@ -5,9 +5,16 @@ function Account() {
     const [expand, setExpand] = useState(false);
     return (
         <div className="account">
-					<div style={{display:'flex'}}>
-						<div className='picture'>Картинка</div>
-						<div className='computer_name'>Computer</div>
+					<div style={{display:'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+						<div className='computer'>
+							<div style={{display: 'flex', gap: 20}}>
+								<div className='invNumber'>#40044004</div>
+								<div className='person'>Юрій Дзюбак</div>
+								<div className='person'>22 аудиторія</div>
+							</div>
+							<div className='computer_name'>Моноблок Apple iMac 24" М1 4.5К 7‑ядер GPU 256GB Green (MJV83UA/A)</div>
+						</div>
+
 						<button onClick={() => setExpand(!expand)}>Button</button>
 					</div>
         {expand &&    
